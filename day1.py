f8d6d19ceaff4bebc01ca58f8c22109d56a8ad9e
Anuja <anujanikumbh5@GMAIL.COM>
2022-04-05 11:55:14 +0530

    printing numbers(even/odd)using for loop

---
+++ day1.py
@@ -0,0 +1,26 @@
+#print 1 to 100 numbers using foor loop
+
+list = []
+for i in range(1,101):
+    list.append(i)
+print(list)
+
+
+#even numbers
+list = []
+for i in range(1, 101):
+   if i%2==0:
+      list.append(i)
+print(list)
+    
+
+#odd numbers
+list = []
+for i in range(1, 101):
+   if i % 2 != 0:
+      list.append(i)
+print(list)
+
+
+               
+       
